527215bcc4731fe70c3ff11169fe1791a92caf6a
TheRealMesteven <mestevenyoutubing@gmail.com>
2025-05-11 22:12:49 +0100

    Update Template and Solution File

--- Template/Properties/AssemblyInfo.cs
+++ Template/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid([MOD_GUID])]
+[assembly: Guid("[MOD_GUID]")]
 
 // Version information for an assembly consists of the following four values:
 //
