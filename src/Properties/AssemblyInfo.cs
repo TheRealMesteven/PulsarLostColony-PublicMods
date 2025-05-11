ac0001523db1642da81cc97e720a31b3c16dde12
TheRealMesteven <mestevenyoutubing@gmail.com>
2025-05-11 22:13:05 +0100

    Update empty project based on Template changes

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid(e6e7a11b-c1ae-4354-bb56-3569edcb989e)]
+[assembly: Guid("e6e7a11b-c1ae-4354-bb56-3569edcb989e")]
 
 // Version information for an assembly consists of the following four values:
 //
